8a55bc805d0bc1fcbd8c41c64e4d9d64568a4c39
Rohin Bhat <bhatrohin003@gmail.com>
2021-04-15 13:00:55 +0530

    Delete consts.dart

--- lib/constants/consts.dart
+++
@@ -1,5 +0,0 @@
-const String BASE_URL = '192.168.43.208:3000';
-const String ADMIN_SIGNIN_URL = 'admin/signin';
-const String ADMIN_SIGNUP_URL = 'admin/signup';
-const String USER_SIGNIN_URL = 'user/signin';
-const String USER_SIGNUP_URL = 'user/signup';
